889cc342bcda0072801cdbe97f2e4a7c495be582
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2023-03-07 20:50:30 +0500

    Configure rspec

---
+++ spec/support/place_helpers.rb
@@ -0,0 +1,15 @@
+module PlaceHelpers
+  def amsterdam_center
+    { lat: 52.35744523071499, lng: 4.880759414295106 }
+  end
+
+  # within 10 kms of amsterdam_center
+  def amsterdam_groenelaan
+    { lat: 52.293732208267784, lng: 4.871389798999393 }
+  end
+
+  # outside 10 kms of amsterdam_center
+  def amsterdam_hofland
+    { lat: 52.21294016354059, lng: 4.8639129520674995 }
+  end
+end
