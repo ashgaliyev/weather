80efed5a77448555b118ccc64eaaa12412f0de8f
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2023-03-09 02:57:48 +0500

    Add weather fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.first
+
+if user
+  if user.places.exists?(name: 'Uralsk')
+    puts "User already has Uralsk\n"
+    exit
+  end
+
+  puts "Seeding user #{user.name}..."
+  forecast_contents = JSON.parse(File.read('spec/fixtures/forecast.json'))
+  weather_contents = JSON.parse(File.read('spec/fixtures/weather.json'))
+
+  puts 'Creating forecast... '
+  forecast = Forecast.create!(
+    lat: weather_contents['coord']['lat'],
+    lng: weather_contents['coord']['lon'],
+    current: weather_contents,
+    five_days: forecast_contents
+  )
+
+  puts 'Creating place... '
+  place = Place.create!(
+    name: 'Uralsk',
+    lat: weather_contents['coord']['lat'],
+    lng: weather_contents['coord']['lon'],
+    forecast: forecast
+  )
+
+  puts 'Creating user place... '
+  user.places << place
+  puts 'Done!'
+else
+  puts 'No users found'
+end
