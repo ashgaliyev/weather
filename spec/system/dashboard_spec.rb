d3f5c6ce45cf854f46db0b5fb8a596b3b5a2063a
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2023-03-10 18:45:22 +0500

    Create dashboard e2e test

---
+++ spec/system/dashboard_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe 'Dashboard screen', type: :system do
+  before do
+    stub_request(:get, /api.openweathermap.org\/data\/2.5\/weather/)
+      .to_return(status: 200, body: File.read(Rails.root.join("spec", "fixtures", "weather.json")), headers: {})
+
+    stub_request(:get, /api.openweathermap.org\/data\/2.5\/forecast/)
+      .to_return(status: 200, body: File.read(Rails.root.join("spec", "fixtures", "forecast.json")), headers: {})
+  end
+
+  it 'shows the dashboard screen' do
+    visit root_path
+    expect(page).to have_content("My Places")
+    expect(page).to have_content("Guest")
+  end
+
+  it 'adds a new place' do
+    create_place
+
+    visit root_path
+
+    expect(page).to have_content("Amsterdam")
+    expect(page).to have_content("3°C..3°C")
+    expect(page).to have_content("8.44 m/s")
+  end
+
+  it 'shows the place screen' do
+    create_place
+
+    visit root_path
+
+    find("[data-test-id='place-card']").click 
+    expect(page).to have_current_path(place_path(Place.last))
+    expect(page).to have_content("Amsterdam")
+  end
+
+  it 'edit a place' do
+    create_place
+
+    visit root_path
+
+    find("[data-test-id='place-card']").click
+    click_on 'Edit'
+    expect(page).to have_current_path(edit_place_path(Place.last))
+    fill_in "name", with: "Amsterdam2"
+    find("[data-test-id='update-place']").click
+    expect(page).to have_current_path(root_path)
+    expect(page).to have_content("Amsterdam2")
+  end
+
+
+  def create_place
+    visit root_path
+    find("[data-test-id='add-place']").click
+
+    expect(page).to have_current_path(new_place_path)
+
+    fill_in "name", with: "Amsterdam"
+    fill_in "lat", with: "52.370216"
+    fill_in "lng", with: "4.895168"
+    
+    find("[data-test-id='create-place']").click
+    expect(page).to have_current_path(root_path)
+  end
+end
