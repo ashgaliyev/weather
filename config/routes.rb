cbe673ab2fb015d8dd7a749451c7accbed234dcd
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2023-03-10 13:35:41 +0500

    fixup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get '/login', to: redirect('/auth/google_oauth2')
   delete '/logout', to: 'sessions#destroy'
+  put '/update_temp_unit', to: 'sessions#update_temp_unit'
 
   get '/forecasts/current', to: 'forecasts#current'
   get '/forecasts/five_days/:place_id', to: 'forecasts#five_days'
